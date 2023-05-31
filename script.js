e82224e5f54f3100794cfa464842c875565d8987
Divya Sandip Malkar <83390741+divi9088@users.noreply.github.com>
2023-05-31 20:32:47 +0530

    Add files via upload
    Responsive Web design is the approach that suggests that design and development should respond to the user's behavior and environment based on screen size, platform and orientation. The practice consists of a mix of flexible grids and layouts, images and an intelligent use of CSS media queries.

---
+++ script.js
@@ -0,0 +1,4 @@
+
+const year = new Date().getFullYear()
+// document.getElementById('copyright-section').appendChild(document.createTextNode(new Date(2025,11).getFullYear()))
+document.getElementById('copyright-section').appendChild(document.createTextNode(new Date().getFullYear()))
